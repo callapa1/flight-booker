7032738607ea4a4b631f1c8bf233fc9c85f6760a
callapa1 <54779545+callapa1@users.noreply.github.com>
2021-03-01 15:57:44 +0100

    Improved date search system and added basic styling

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,14 +2,14 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
     if params[:search].present?
-      @code = params[:search][:code]
+      @code = params[:search][:code].upcase
       @from = params[:search][:from_airport_id]
       @to = params[:search][:to_airport_id]
       if params[:search][:date] != ''
-        @date = params[:search][:date]
+        @flights = Flight.where("cast(date as DATE) = ? ", params[:search][:date] )
+      else
+        @flights = Flight.where("code = ? OR (from_airport_id = ? AND to_airport_id = ?)", @code, @from, @to)
       end
-      # inside where goes raw SQL
-      @flights = Flight.where("code = ? OR (from_airport_id = ? AND to_airport_id = ?) OR date = ?", @code, @from, @to, @date)
       @booking = Booking.new
     end
   end
