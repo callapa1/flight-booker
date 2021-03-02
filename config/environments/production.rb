0e04cf6d49fb36f4137a58b4d4513fd46acbed3d
callapa1 <54779545+callapa1@users.noreply.github.com>
2021-03-02 15:45:40 +0100

    Config env prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -101,13 +101,15 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
+    # domain: 'gmail.com',
     authentication: :login,
-    enable_starttls_auto: true,
+    # enable_starttls_auto: true,
     user_name: ENV['GOOGLE_FLIGHT_USERNAME'],
     password: ENV['GOOGLE_FLIGHT_PASSWORD']
   }
 
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'tranquil-chamber-45428.herokuapp.com', protocol: 'https' }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
