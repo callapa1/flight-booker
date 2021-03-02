d290c69285da2fddb4af02c6a6b9906e8af7bc7e
callapa1 <54779545+callapa1@users.noreply.github.com>
2021-03-02 14:40:12 +0100

    Config env prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  host: 'herokuapp.com'
+  host: 'tranquil-chamber-45428.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
